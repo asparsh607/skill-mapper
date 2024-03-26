4cedc334ef6fd284df6c2c7581e3eee51a5403c5
asparsh607 <113459577+asparsh607@users.noreply.github.com>
2024-03-26 22:57:48 +0530

    Update main.py, added the code to write the current directory location to use in pbix file's FileName table.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import os
 from extract_jobs import scrape_jobs, format_string
 from transform_jobs import transform_jobs
 from config_files.countries import it_hubs_by_country
@@ -18,4 +19,7 @@ if __name__ == "__main__":
     print("Process Completed!")
     print("Please open job_analytics.pbix file to see Visualisation.")
     print("Do no forget to refresh the data!")
+    current_directory = os.getcwd()
+    with open('current_directory_location.txt', 'w', encoding='utf-8') as cur_dir:
+        cur_dir.write(str(current_directory)+'\job_data\job_data.csv')
     time.sleep(10)
